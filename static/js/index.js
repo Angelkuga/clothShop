6c4618c970145db8ba34cdb23592a84e3647f0df
kuga <jingdemei@noahwm.com>
2017-04-13 20:28:00 +0800

    首页  移动端

---
+++ static/js/index.js
@@ -0,0 +1,124 @@
+var menuAniData= {
+	open: 600,
+	close: 800
+};
+var menuTimer;
+
+function findBootstrapEnvironment() {
+    var envs = ['xs', 'sm', 'md', 'lg'];
+
+    var $el = $('<div>');
+    $el.appendTo($('body'));
+
+    for (var i = envs.length - 1; i >= 0; i--) {
+        var env = envs[i];
+
+        $el.addClass('hidden-'+env);
+        if ($el.is(':hidden')) {
+            $el.remove();
+            return env;
+        }
+    }
+}
+
+function setFootMenu() {
+	console.log(findBootstrapEnvironment);
+	if (findBootstrapEnvironment() == "xs" || findBootstrapEnvironment() == "sm") {
+		$("#j_footer_menu").addClass("footer-menu-mobile");
+		$("#j_footer_menu .sub-menu .wrap-box").css("display", "none");
+		$("#j_footer_menu .sub-menu .tit").unbind().click(function() {
+			console.log("come in");
+			$("#j_footer_menu .sub-menu .tit").removeClass("opened");
+			if ($(this).closest("li").find(".wrap-box").css("display") == "block") {
+				$(this).removeClass("opened");
+				$(this).closest("li").find(".wrap-box").removeClass("opened")
+			} else {
+				$(this).addClass("opened");
+				$(this).closest("li").find(".wrap-box").addClass("opened")
+			}
+			return false
+		})
+	} else {
+		$("#j_footer_menu").removeClass("footer-menu-mobile");
+		$("#j_footer_menu .sub-menu .tit").unbind("click");
+		$("#j_footer_menu .sub-menu .wrap-box").css("display", "block")
+	}
+}
+
+
+
+function bindEvent() {
+	$('.menu-toggle').click(function(){
+		$('.sub-menu-sm-xs').toggleClass('opened');
+	});
+
+	$('.sub-menu-sm-xs .close').click(function() {
+		$('.sub-menu-sm-xs').removeClass('opened');
+	});
+
+	$('.sub-menu-sm-xs a.main').click(function(){
+		var $a = $(this);
+		var $ul = $(this).next('ul');
+		$a.removeClass("opened");
+		if ($ul.css("display") == "block") {
+			 $a.removeClass("opened");
+			 $ul.slideUp();
+		} else {
+			$a.addClass("opened");
+			$ul.slideDown();
+		}
+		return false
+	});
+
+
+	$(".menu-item").on("mouseleave", function() {
+		var $menuItem = $(this);
+		clearTimeout(menuTimer);
+		hideSubMenu($menuItem);
+		menuTimer = setTimeout(function() {
+			$menuItem.find(".title").removeClass("selected")
+		}, menuAniData.close)
+	});
+
+	$(".menu-item").on("mouseover", function() {
+		var $menuItem = $(this);
+		$menuItem.find(".title").removeClass("selected");
+		$menuItem.find(".title").addClass("selected");
+		showSubMenu($menuItem);
+		clearTimeout(menuTimer);
+		menuTimer = setTimeout(function() {			
+		}, menuAniData.open)
+	});
+	
+	// $(".menu-item").find(".title").on("click", function(A) {
+	// 	if (!$("html").hasClass("touch")) {
+	// 		clearTimeout(x);
+	// 		var $menuItemTitle = $(this);
+	// 		var $mainMenu= $menuItemTitle.closest(".main-menu");
+	// 		var $menuItem = $menuItemTitle.closest(".menu-item");
+	// 		$menuItem.addClass("selected")
+	// 	}
+	// });
+
+}
+
+function hideSubMenu(target) {
+	target.find(".sub-menu").removeClass("show");
+	target.find(".title").removeClass("selected");
+	if (target.hasClass("mainmenu")) {
+		target.removeClass("open")
+	} else {
+		target.closest(".mainmenu").removeClass("open")
+	}
+}
+
+function showSubMenu(target) {
+	target.closest(".mainmenu").addClass("open");
+	$('.main-menu').find(".sub-menu").removeClass("show");
+	target.find(".sub-menu").addClass("show");
+	target.find(".title").addClass("selected")
+}
+$(document).ready(function() {
+	setFootMenu();
+	bindEvent();
+});
